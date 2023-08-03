a8622f45652c0af1e81ec9ea78a5a2193bce2a9a
“camelier” <camelier@gmail.com>
2023-08-03 09:58:56 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// toogleMode já altera a classe em classList
+function toggleMode() {
+  //const ou var são "apelidos"
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //Pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //Se tiver em "light" mode, substituir o link da img pela img light
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
